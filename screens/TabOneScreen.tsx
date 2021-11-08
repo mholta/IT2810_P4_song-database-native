ae90762a38de9eb26ae4be000c84d9f7d62d41fc
Tormod Nygård <tormodny@stud.ntnu.no>
2021-11-08 12:18:02 +0100

    Fetches all songs from DB #2

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,44 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { useQuery, gql } from "@apollo/client";
+import * as React from "react";
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const options = {
+    variables: {
+      searchString: "",
+      themes: undefined,
+      contributor: undefined,
+      sortOrder: "asc",
+      sortType: "releaseDate",
+    },
+  };
+
+  const { loading, error, data } = useQuery(GET_SEARCH_RESULTS, {
+    ...options,
+    variables: {
+      ...options?.variables,
+      limit: 20,
+      page: 1,
+    },
+  });
+
+  console.log(loading);
+  console.log(data);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -18,16 +47,48 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
+
+const GET_SEARCH_RESULTS = gql`
+  query Songs(
+    $searchString: String
+    $themes: [String!]
+    $contributor: String
+    $sortOrder: SortOrder!
+    $sortType: SortType!
+    $page: Int!
+    $limit: Int!
+  ) {
+    songs(
+      searchString: $searchString
+      filter: { categories: $themes, contributor: $contributor }
+      limit: $limit
+      page: $page
+      sorting: { order: $sortOrder, sortType: $sortType }
+    ) {
+      songs {
+        _id
+        artists {
+          name
+        }
+        title
+        album {
+          picture
+        }
+      }
+      pages
+    }
+  }
+`;
