69e3916f6b502cf671760745b582fe52e9cd6724
Håkon Nese <haaknes@stud.ntnu.no>
2021-11-16 08:47:57 +0100

    Extend native datepicker #3

--- components/SubmitSong/DatePicker.native.tsx
+++ components/SubmitSong/DatePicker.native.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { useState } from "react";
-import { View, Platform } from "react-native";
+import { View, Platform, Button } from "react-native";
 import NativeDatePicker from "@react-native-community/datetimepicker";
 import { DatePickerProps } from "./DatePicker";
 
 const DatePicker = ({ onChange, value }: DatePickerProps) => {
+  const [show, setShow] = useState(false);
   if (Platform.OS !== "web") {
     return (
       <View>
-        <NativeDatePicker
-          accessibilityLabel="Choose date"
-          value={value ?? new Date()}
-          onChange={(_: any, selectedDate?: Date) => {
-            selectedDate && onChange(selectedDate);
-          }}
-          mode="date"
+        <Button
+          onPress={() => setShow(true)}
+          title={value?.toLocaleDateString() ?? "Velg utgivelsesdato"}
         />
+        {show && (
+          <NativeDatePicker
+            accessibilityLabel="Choose date"
+            value={value ?? new Date()}
+            onChange={(_: any, selectedDate?: Date) => {
+              setShow(Platform.OS === "ios");
+              selectedDate && onChange(selectedDate);
+            }}
+            mode="date"
+          />
+        )}
       </View>
     );
   }
